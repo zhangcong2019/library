b954c2af8fe767c132936f35d8c8a3a6b5512ba8
cong <cong1.zhang@intel.com>
2022-08-12 14:36:15 +0800

    c call c++ shared library

---
+++ example5/lib1.c
@@ -0,0 +1,8 @@
+#include "lib1.h"
+
+#include <stdio.h>
+
+void fun1 ( int x )
+{
+    printf("%i\n",x);
+}
---
+++ example5/lib2.cpp
@@ -0,0 +1,10 @@
+#include "lib2.h"
+
+#include <stdio.h>
+#include <map>
+
+void fun2 ( int x )
+{
+    std::map<int, int> map;
+    printf("%i\n",2*x);
+}
